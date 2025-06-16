b848a8e4dcd758ff5e08a0b7a7c14bc1788e8761
passwordd009 <elijah.hawes47@gmail.com>
2025-06-16 15:11:23 -0400

    changed repo

--- Hestia_Frontend/src/pages/Home.jsx
+++ Hestia_Frontend/src/pages/Home.jsx
@@ -1,8 +1,45 @@
 const Home = () => {
-    return(
-       <>
-       </>
-    )
-   }
-   
-   export default Home
+   return(
+      <>
+      <div id = "container" style={containerStyle1}>
+        <div>
+           <h2>Earn Points. Get Rewards. Power Your Community.</h2>
+           <center>
+              <h5>Join activities, scan QR codes, and redeem discounts at local vendors.</h5>
+           </center>
+        </div>
+
+        <div>
+
+        </div>
+
+        <div>
+
+        </div>
+
+        <div>
+
+        </div>
+      </div>
+      </>
+   )
+  }
+
+  const containerStyle1 = {
+     position: "relative",
+     width: "100vw",
+     height: "100vh",
+     margin: "0 auto",
+     display: "flex",
+     gap: '50px',
+     flexDirection: 'column',
+     justifyContent: "center",
+     alignItems: "center",
+   };
+  
+  export default Home
+  //Should Introduce the app
+  //The page should have the goal
+  //The page should show the vendors 
+  //A few of the rewards
+  //The page should show a list of Activitys
